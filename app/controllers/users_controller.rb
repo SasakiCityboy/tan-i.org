605d87e518b3f8e5e9f4f39ec071dd6e64ecbb57
SasakiCityboy <sasaki.cityboy@gmail.com>
2016-01-08 21:21:00 +0900

    ミス修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
-      # @nickname = current_user.nickname
-      # @tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+  def show
+      @nickname = current_user.nickname
+      @tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+  end
 end
