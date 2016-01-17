0eae8c863f09f8208f8c33b31afc1932ef10d76e
SasakiCityboy <sasaki.cityboy@gmail.com>
2016-01-17 19:05:11 +0900

    no message

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,29 +13,31 @@ class TweetsController < ApplicationController
     Tweet.create(name: tweet_params[:name], tani: tweet_params[:tani], text: tweet_params[:text], user_id: current_user.id)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
+  end
+
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+  end
+
   private
   def tweet_params
     params.permit(:name, :tani, :text)
   end
 
-    def destroy
-      tweet = Tweet.find(params[:id])
-      if tweet.user_id == current_user.id
-        tweet.destroy
-      end
-    end
-
-    def edit
-      @tweet = Tweet.find(params[:id])
-    end
-
-
-    def update
-      tweet = Tweet.find(params[:id])
-      if tweet.user_id == current_user.id
-        tweet.update(tweet_params)
-      end
-    end
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
